41b4aa1a521c505a1df342ba4047f38cd1af17b8
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-19 14:29:03 +0200

    formatted

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, FormEvent, useContext, useState } from 'react';
-import './Modal.scss';
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import "./Modal.scss";
 import ReactDOM from "react-dom";
-import { AppContext } from '../../Context/AppContext';
-import Button from '../Button/Button';
-import { Employee } from '../../Context/hooks/useAppContext';
-import { t } from 'i18next';
+import { AppContext } from "../../Context/AppContext";
+import Button from "../Button/Button";
+import { Employee } from "../../Context/hooks/useAppContext";
+import { t } from "i18next";
 
 type ModalProps = {
   onClose: () => void;
@@ -30,7 +30,7 @@ const Modal = ({
   weight,
   hair,
   eyeColor,
-  ip
+  ip,
 }: ModalProps) => {
   const modalRoot = document.getElementById("modal-root");
   const { updateEmployees } = useContext(AppContext);
@@ -124,67 +124,137 @@ const Modal = ({
     setCity(tempCity);
 
     onClose();
-  }
-  return modalRoot ? ReactDOM.createPortal(
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <h2 className='modal__title'><b>{t("app.full-name")} </b>{firstName} {lastName}</h2>
-        <section className='modal__section'>
-          <div className='modal__section-box'>
-            <img src={image} alt={firstName} />
+  };
+  return modalRoot
+    ? ReactDOM.createPortal(
+        <div className="modal-overlay" onClick={onClose}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <h2 className="modal__title">
+              <b>{t("app.full-name")} </b>
+              {firstName} {lastName}
+            </h2>
+            <section className="modal__section">
+              <div className="modal__section-box">
+                <img src={image} alt={firstName} />
+              </div>
+
+              <div className="modal__section-box">
+                <p>
+                  <b>{t("app.user-name")}:</b> {username}
+                </p>
+                <p>
+                  <b>{t("app.email")}:</b> {email}
+                </p>
+                <p>
+                  <b>{t("app.phone")}:</b> {phone}
+                </p>
+                <p>
+                  <b>{t("app.ip")}:</b> {ip}
+                </p>
+                <p>
+                  <b>{t("app.password")}:</b> {password}
+                </p>
+              </div>
+
+              <div className="modal__section-box">
+                <p>
+                  <b>{t("app.birth-date")}:</b> {birthDate}
+                </p>
+                <p>
+                  <b>{t("app.age")}:</b> {age}
+                </p>
+                <p>
+                  <b>{t("app.maiden-name")}:</b> {maidenName}
+                </p>
+                <p>
+                  <b>{t("app.gender")}:</b> {gender}
+                </p>
+              </div>
+
+              <div className="modal__section-box">
+                <p>
+                  <b> {t("app.blood-group")}:</b> {bloodGroup}
+                </p>
+                <p>
+                  <b> {t("app.height")}:</b> {height}cm
+                </p>
+                <p>
+                  <b> {t("app.weight")}:</b> {weight}kg
+                </p>
+                <p>
+                  <b> {t("app.eye-color")}:</b> {eyeColor}
+                </p>
+              </div>
+
+              <div className="edit-worker">
+                <h2 className="edit-worler__title">Edytuj Pracownika</h2>
+                <form className="edit-worker__form" onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    value={tempFirstName}
+                    onChange={handleFirstNameChange}
+                    name="firstName"
+                    placeholder="Imię"
+                  />
+                  <input
+                    type="text"
+                    value={tempLastName}
+                    onChange={handleLastNameChange}
+                    name="lastName"
+                    placeholder="Nazwisko"
+                  />
+                  <input
+                    type="date"
+                    value={tempBirthDate}
+                    onChange={handleBirthDateChange}
+                    name="birthDate"
+                    placeholder="Data urodzenia"
+                  />
+                  <input
+                    type="text"
+                    value={tempAddress}
+                    onChange={handleAddressChange}
+                    name="address"
+                    placeholder="Ulica"
+                  />
+                  <input
+                    type="text"
+                    value={tempCity}
+                    onChange={handleCityChange}
+                    name="city"
+                    placeholder="Miejscowość"
+                  />
+                  <input
+                    type="email"
+                    value={tempEmail}
+                    onChange={handleEmailChange}
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <input
+                    type="tel"
+                    value={tempPhone}
+                    onChange={handlePhoneChange}
+                    name="phone"
+                    placeholder="Numer telefonu"
+                  />
+                  <input
+                    type="url"
+                    value={tempImage}
+                    onChange={handleImageChange}
+                    name="image"
+                    placeholder="URL do zdjęcia"
+                  />
+                  <Button label="Edit" type="submit" />
+                  <Button label={t("app.close")} onClick={onClose} />
+                </form>
+              </div>
+            </section>
           </div>
-
-          <div className="modal__section-box">
-
-            <p><b>{t("app.user-name")}:</b> {username}</p>
-            <p><b>{t("app.email")}:</b> {email}</p>
-            <p><b>{t("app.phone")}:</b> {phone}</p>
-            <p><b>{t("app.ip")}:</b> {ip}</p>
-            <p><b>{t("app.password")}:</b> {password}</p>
-          </div>
-
-          <div className="modal__section-box">
-
-            <p><b>{t("app.birth-date")}:</b> {birthDate}</p>
-            <p><b>{t("app.age")}:</b> {age}</p>
-            <p><b>{t("app.maiden-name")}:</b> {maidenName}</p>
-            <p><b>{t("app.gender")}:</b> {gender}</p>
-          </div>
-
-          <div className="modal__section-box">
-
-            <p><b> {t("app.blood-group")}:</b> {bloodGroup}</p>
-            <p><b> {t("app.height")}:</b> {height}cm</p>
-            <p><b> {t("app.weight")}:</b> {weight}kg</p>
-            <p><b> {t("app.eye-color")}:</b> {eyeColor}</p>
-          </div>
-
-    
-
-          <div className="edit-worker">
-            <h2 className="edit-worler__title">Edytuj Pracownika</h2>
-            <form className="edit-worker__form" onSubmit={handleSubmit}>
-
-
-              <input type="text" value={tempFirstName} onChange={handleFirstNameChange} name="firstName" placeholder="Imię" />
-              <input type="text" value={tempLastName} onChange={handleLastNameChange} name="lastName" placeholder="Nazwisko" />
-              <input type="date" value={tempBirthDate} onChange={handleBirthDateChange} name="birthDate" placeholder="Data urodzenia" />
-              <input type="text" value={tempAddress} onChange={handleAddressChange} name="address" placeholder="Ulica" />
-              <input type="text" value={tempCity} onChange={handleCityChange} name="city" placeholder="Miejscowość" />
-              <input type="email" value={tempEmail} onChange={handleEmailChange} name="email" placeholder="Email" />
-              <input type="tel" value={tempPhone} onChange={handlePhoneChange} name="phone" placeholder="Numer telefonu" />
-              <input type="url" value={tempImage} onChange={handleImageChange} name="image" placeholder="URL do zdjęcia" />
-              <Button label='Edit' type='submit' />
-              <Button label={t("app.close")} onClick={onClose} />
-            </form>
-          </div>
-        </section>
-      </div>
-
-
-    </div >,
-    modalRoot
-  ) : null;
+        </div>,
+        modalRoot
+      )
+    : null;
 };
 
 export default Modal;
