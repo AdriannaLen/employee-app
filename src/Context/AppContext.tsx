f965bf9925c988d343f2d6c151631129658554b9
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-24 14:25:55 +0200

    added pagination state to context

--- src/Context/AppContext.tsx
+++ src/Context/AppContext.tsx
@@ -1,16 +1,16 @@
 import React, { createContext,PropsWithChildren, FC, useState, useEffect } from 'react';
 import useAppContext, { Employee } from "./hooks/useAppContext";
 
-// type Props = {
-// children: JSX.Element | JSX.Element[];
-// };
-
 type EmployeeContextType = {
   employees: Employee[];
   isLoading: boolean;
   setEmployees: React.Dispatch<React.SetStateAction<Employee[]>>;
   updateEmployees: (id: number, updateData: Employee) => void;
   deleteEmployee: (id: number) => void;
+  currentPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  setCurrentPage: (page: number) => void;
 };
 export const AppContext = createContext<EmployeeContextType>(
   {} as EmployeeContextType
@@ -19,11 +19,17 @@ export const AppContext = createContext<EmployeeContextType>(
 export const AppContextProvider: FC<PropsWithChildren> = ({children}) => {
   const { fetchedEmployees, isLoading, updateEmployees, deleteEmployee } = useAppContext();
   const [employees, setEmployees] = useState<Employee[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+
   useEffect(() => {
     if (fetchedEmployees.length > 0) {
       setEmployees(fetchedEmployees);
     }
   }, [fetchedEmployees]);
+
+  const totalPages = Math.ceil(employees.length / itemsPerPage);
+
   return (
     <AppContext.Provider 
     value={{ 
@@ -31,7 +37,11 @@ export const AppContextProvider: FC<PropsWithChildren> = ({children}) => {
       isLoading, 
       setEmployees, 
       updateEmployees, 
-      deleteEmployee 
+      deleteEmployee,
+      currentPage,
+      itemsPerPage,
+      totalPages,
+      setCurrentPage,
       }}
       >
       {children}
